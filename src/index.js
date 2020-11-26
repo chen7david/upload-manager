4ce204c084e20f9b03fe0e4864c7e82056c3ed2e
David <chen7david@me.com>
2020-11-26 22:30:27 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const Koa = require('koa')
 const app = new Koa() 
 const cors = require('kcors')
 const { server } = require('config')
+const multer = require('@koa/multer')
+const upload = multer()
 const url = require('url')
 
 const router = require('koa-router')
