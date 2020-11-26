8ecc394f8fd7f5e07932fb56f398edd5327911a6
David <chen7david@me.com>
2020-11-26 22:25:58 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const Koa = require('koa')
+const app = new Koa() 
+const cors = require('kcors')
+const { server } = require('config')
+const url = require('url')
+
+const router = require('koa-router')
+
+/* MIDDLEWARE */
+app.use(cors())
+
+app.listen(server.port, () => {
+    console.log(`running at ${url.format(server)}`)
+})
